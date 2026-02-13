3dc97ab5347983d226c45481eef95c568270f319
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-13 22:39:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click",function(e)){
+     e.preventDefault();
+	let p1 = document.getElementById("p1").value;
+	let p2 = document.getElementById("p2").value;
+
+	document.querySelector("form").style.display = "none";
+     
+	let container = document.querySelector(".container");
+	let h1 = document.querySelector("h1");
+	h1.textContent = "Tic Tac Toe";
+	container.appendChild(h1);
+	
+	
+}
+
